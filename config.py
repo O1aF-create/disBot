a4c485ec6c0dcbe7e492759366d1801d0819d099
O1aF-create <60196637+O1aF-create@users.noreply.github.com>
2020-08-12 23:45:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+TOKEN = 'NzQyMzI3NzYzNzQ2MTYwNjUw.XzEgmg.HTiHC3NSBHQ40Ym_-KBcbeanb0I'
+
+POST_ID = 742810925223903283 # post id to read reactions from
+
+# roles list according to emotes
+ROLES = {
+    '🎮': 692131597524271134, # dokerbl
+    '🤖': 551077989924732928, # Moderators
+    '👩🏻': 686181264025321484, # Ledi
+    '👺': 555114928403644436, # CHUDISHE
+}
+
+# exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 2 # max ammount of roles a user can have
